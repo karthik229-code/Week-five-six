61803479503761e8de77b8f00b0c9b48083c72b5
karthikghnana <karhikghnana@gmail.com>
2026-03-17 21:19:10 +0530

    takes inputs and calculate bmi required number of times

---
+++ Level-2/bmi.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class bmi {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of persons: ");
+        int n = sc.nextInt();
+
+        double[] height = new double[n];
+        double[] weight = new double[n];
+        double[] bmi = new double[n];
+        String[] status = new String[n];
+
+        for (int i = 0; i < n; i++) {
+            System.out.print("Enter height (m): ");
+            height[i] = sc.nextDouble();
+
+            System.out.print("Enter weight (kg): ");
+            weight[i] = sc.nextDouble();
+        }
+
+        for (int i = 0; i < n; i++) {
+            bmi[i] = weight[i] / (height[i] * height[i]);
+
+            if (bmi[i] <= 18.4)
+                status[i] = "Underweight";
+            else if (bmi[i] <= 24.9)
+                status[i] = "Normal";
+            else if (bmi[i] <= 39.9)
+                status[i] = "Overweight";
+            else
+                status[i] = "Obese";
+        }
+
+        for (int i = 0; i < n; i++) {
+            System.out.println("Person " + (i+1) + ": BMI = " + bmi[i] + " Status = " + status[i]);
+        }
+    }
+}
