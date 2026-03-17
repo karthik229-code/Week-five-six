1753102b8c929c8ea4cb5b5d3691d38e53ea40b7
karthikghnana <karhikghnana@gmail.com>
2026-03-17 20:20:14 +0530

    takes elements of 2D array and converts to 1D

---
+++ Level-1/TwodToOned.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class TwodToOned {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter rows: ");
+        int rows = sc.nextInt();
+
+        System.out.print("Enter columns: ");
+        int cols = sc.nextInt();
+
+        int[][] matrix = new int[rows][cols];
+
+        // Input 2D array
+        System.out.println("Enter elements:");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+
+        // Convert to 1D
+        int[] arr = new int[rows * cols];
+        int index = 0;
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                arr[index++] = matrix[i][j];
+            }
+        }
+
+        // Print 1D array
+        System.out.println("1D Array:");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
