1c3eaaf30eaf03f428f020120e1154d778ffae11
karthikghnana <karhikghnana@gmail.com>
2026-03-17 20:06:24 +0530

    takes a number as input and gives multiplication table using arrays

---
+++ Level-1/multiplication.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class multiplication {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        int[] table = new int[10];
+
+        // Store results
+        for (int i = 0; i < 10; i++) {
+            table[i] = num * (i + 1);
+        }
+
+        // Display
+        for (int i = 0; i < 10; i++) {
+            System.out.println(num + " * " + (i + 1) + " = " + table[i]);
+        }
+    }
+}
